737fb314022b9990cb8bb299e35e951a09711b2f
damion-w <67649457+damion-w@users.noreply.github.com>
2020-08-31 13:04:47 -0700

    removed index, as it is not needed

--- grocery-tracker-app/controllers/users-controller.js
+++ grocery-tracker-app/controllers/users-controller.js
@@ -2,24 +2,6 @@ const bcrypt = require('bcryptjs')
 const User = require('../models/users-model')
 
 const usersController = {
-  index(req, res, next) {
-    req.user
-      .findUserPantry()
-      .then((groceries) => {
-        //working response of user's added groceries
-        // res.json({
-        //   message: 'user profile on this route',
-        //   data: {
-        //     user: req.user,
-        //     groceries,
-        //   },
-        // })
-        //res.render('/userPantry')
-        res.redirect('/pantry')
-      })
-      .catch(next)
-  },
-
   create(req, res, next) {
     const salt = bcrypt.genSaltSync()
     const hash = bcrypt.hashSync(req.body.password, salt)
