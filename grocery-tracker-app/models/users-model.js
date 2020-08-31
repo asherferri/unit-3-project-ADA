6029ea9185010453664dc55c5d51f10d18437a6e
damion-w <67649457+damion-w@users.noreply.github.com>
2020-08-31 13:01:08 -0700

    update snake case to camel, update module.exports

--- grocery-tracker-app/models/users-model.js
+++ grocery-tracker-app/models/users-model.js
@@ -1,12 +1,13 @@
 //we need the name of the model for tracked groceries model.
 const db = require('../db/config')
+const Groceries = require('../models/groceries-model')
 const NameOfGroceriesModel = require('./tracked-groceries-model')
 
 class Users {
-    constructor({id, first_name, last_name, email, username, password_digest}) {
+    constructor({id, firstName, lastName, email, username, password_digest}) {
         this.id = id || null
-        this.first_name = first_name
-        this.last_name = last_name
+        this.firstName = firstName
+        this.lastName = lastName
         this.email = email
         this.username = username
         this.password_digest = password_digest
@@ -17,7 +18,7 @@ class Users {
         .oneOrNone('SELECT * FROM users WHERE username = $1', username)
         .then((user) => {
             if(user) return new this(user)
-            else throw new Error('THat User does not exists')
+            else throw new Error('That User does not exists')
         })
     }
 
@@ -29,16 +30,6 @@ class Users {
             RETURNING *`, this)
             .then((savedUser) => Object.assign(this, savedUser))
     }
-
-    //adding new method findUserGroceries
-    findUserGroceries() {
-        return db
-        .manyOrNone('SELECT * FROM launch WHERE user_id = $1', this.id)
-        .then((groceries) => {
-            return groceries.map((item) => new NameOfGroceriesModel(item))
-        })
-    }
-
 }
 
-module.exports = User
+module.exports = Users
