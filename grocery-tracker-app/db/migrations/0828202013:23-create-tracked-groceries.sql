089ceb7da2767845323a704e88864bdb512d952c
Asher Ferri <asher@Ashers-MacBook-Pro.local>
2020-08-31 22:08:11 -0400

    some Functional mods to DB and pckg json on xpress app for heroku dlpoyment.

--- grocery-tracker-app/db/migrations/0828202013:23-create-tracked-groceries.sql
+++ grocery-tracker-app/db/migrations/0828202013:23-create-tracked-groceries.sql
@@ -2,8 +2,7 @@ CREATE TABLE IF NOT EXISTS groceries (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     recurrence INTEGER, 
-    last_purchased_date date,
-    user_id INTEGER NOT NULL FOREIGN KEY REFERENCES users(id)
+    last_purchased_date date
 );
 
 /* Allowing NULL in recurrance and last_purchased_date to account for one-off groceries added to a list */
