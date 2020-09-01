a2798a4598414d359eeb88422101c4f38261a719
Damion Wiltshire <drw.5452@gmail.com>
2020-09-01 09:27:56 -0700

    added route for adding a new grocery

--- grocery-tracker-app/routes/groceries-router.js
+++ grocery-tracker-app/routes/groceries-router.js
@@ -3,6 +3,7 @@ const groceriesController = require('../controllers/groceries-controller')
 const groceriesRouter = require('express').Router()
 
 groceriesRouter.get('/', groceriesController.index)
-groceriesRouter.get('/:id([0-9]+)', groceriesController.show);
+groceriesRouter.post('/', groceriesController.create)
+groceriesRouter.get('/:id([0-9]+)', groceriesController.show)
 
 module.exports = groceriesRouter
