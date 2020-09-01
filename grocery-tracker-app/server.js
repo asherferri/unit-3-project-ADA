71c647f17c0c39f3fbd4339c3f2bfe19df14ec07
Asher Ferri <asher@Ashers-MacBook-Pro.local>
2020-09-01 17:50:12 -0400

    uncomented line 27 server.js app.use(e.s.'public'

--- grocery-tracker-app/server.js
+++ grocery-tracker-app/server.js
@@ -24,7 +24,7 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(methodOverride('_method'))
-// app.use(express.static('public'))
+app.use(express.static('public'))
 app.use(cookieParser())
 // //uncoment this when updating server.js for auth addon
 app.use(
