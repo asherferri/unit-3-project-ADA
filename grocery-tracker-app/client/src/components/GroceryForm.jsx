c2889d09783afcc180be294732e0b180ea95a767
Andrew Moroco <andrew.moroco@gmail.com>
2020-09-01 07:18:06 -0700

    updated groceryform

--- grocery-tracker-app/client/src/components/GroceryForm.jsx
+++ grocery-tracker-app/client/src/components/GroceryForm.jsx
@@ -22,9 +22,19 @@ class GroceryForm extends React.Component {
         return (
             <div className="grocery-form">
                 <form onSubmit={(evt) => this.props.handleFormSubmit('POST', evt, this.state)} >
-                    <input type="text" name="name" placeholder="grocery item" value={this.state.name} onChange={this.handleChange} />
-                    <input type="number" name="recurrence" value={this.state.recurrence} onChange={this.handleChange} />
-                    <input type="date" name="last_purchased_date" value={this.state.last_purchased_date} onChange={this.handleChange} />
+                    <div>
+                        <label>Grocery Item Name:</label>
+                        <input type="text" name="name" placeholder="grocery item" value={this.state.name} onChange={this.handleChange} />
+                    </div>
+                    <div>
+                        <label>Shopping Recurrence:</label>
+                        <input type="number" name="recurrence" value={this.state.recurrence} onChange={this.handleChange} />
+                    </div>
+                    <div>
+                        <label>Last Purchased Date:</label>
+                        <input type="date" name="last_purchased_date" value={this.state.last_purchased_date} onChange={this.handleChange} />
+                    </div>
+                    <input type="submit" value="Submit" />
                 </form>
             </div>
         )
