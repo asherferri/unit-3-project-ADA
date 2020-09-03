851948afa510d20235bae934de3e80dd2d257a56
Damion Wiltshire <drw.5452@gmail.com>
2020-09-03 09:49:04 -0700

    removing userID and leading zeros in dates

--- grocery-tracker-app/client/src/components/GroceryItem.jsx
+++ grocery-tracker-app/client/src/components/GroceryItem.jsx
@@ -7,18 +7,16 @@ const GroceryItem = (props) => {
             <div className="grocery-item">
                 <h3>{props.grocery.name}</h3>
                 {/* Using moment to format date */}
-                <p>Last Purchased: {Moment(props.grocery.last_purchased_date, "dddd, MMMM Do, YYYY").format("MM/DD/YYYY")}</p> 
+                <p>Last Purchased: {Moment(props.grocery.last_purchased_date, "dddd, MMMM Do, YYYY").format("M/D/YYYY")}</p> 
                 <p>Shopping Recurrance: {props.grocery.recurrence} days</p>
                 {/* Using moment to format date and add recurrence */}
-                <p>Expected Re-Purchase: {Moment(props.grocery.last_purchased_date, "dddd, MMMM Do, YYYY").add(props.grocery.recurrence, 'days').format("MM/DD/YYYY")}</p>
-                <p>UserId: {props.grocery.user_id}</p>
+                <p>Expected Re-Purchase: {Moment(props.grocery.last_purchased_date, "dddd, MMMM Do, YYYY").add(props.grocery.recurrence, 'days').format("M/D/YYYY")}</p>
             </div>
             <div className="item-button">
                 {/* Sending ID back up to GroceryController to lookup single grocery to make edits */}
                 <span onClick={() => props.getGrocery(props.grocery.id, 'edit')}>Update</span>
                 {/* Sending ID back up to FeatureWindow to handle deleting grocery from db */}
                 <span onClick={(evt) => props.handleFormSubmit('DELETE', evt, props.grocery, props.grocery.id)} >Delete</span>
-                <span onClick={(evt) => props.handleFormSubmit('DELETE', evt, props.grocery, props.grocery.id)} >Recipe</span>
             </div>
         </div>
     )
