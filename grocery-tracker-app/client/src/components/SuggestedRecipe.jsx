43c49648328abcd8087b989122013d077a244efd
Damion Wiltshire <drw.5452@gmail.com>
2020-09-03 08:32:00 -0700

    recipe instructions appear

--- grocery-tracker-app/client/src/components/SuggestedRecipe.jsx
+++ grocery-tracker-app/client/src/components/SuggestedRecipe.jsx
@@ -6,14 +6,16 @@ class SuggestedRecipe extends Component {
          super()
          this.state = {
              videoTitle: '',
-             ingredients: []
+             ingredients: [],
+             instructions: [],
+             imageUrl: ''
          }
      }
     
     componentDidMount() {
         console.log('Componend mounted!')
 
-        fetch(`https://tasty.p.rapidapi.com/recipes/list?tags=under_30_minutes&q=spinach&from=0&sizes=20`, {
+        fetch(`https://tasty.p.rapidapi.com/recipes/list?tags=under_30_minutes&q=chicken&from=0&sizes=20`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -24,17 +26,16 @@ class SuggestedRecipe extends Component {
         .then(res => res.json())
         .then(res => {
             console.log(res.results[0].name)
-            console.log(res.results[0].sections[0].components);
+            console.log(res.results[0].thumbnail_url);
             console.log('res')
             console.log(res)
             this.setState({
-                videoTitle: res.results[0].name,
-                ingredients: res.results[0].sections[0].components
+              videoTitle: res.results[0].name,
+              ingredients: res.results[0].sections[0].components,
+              instructions: res.results[0].instructions,
+              imageUrl: res.results[0].thumbnail_url
             })
             console.log(this.state.videoTitle);
-            // this.state.ingredients.map((ingredient, i) => {
-            //     console.log(`${i}. ${ingredient.raw_text}`);
-            // }) 
         })
         .catch(err => console.log(err))
 
@@ -45,15 +46,21 @@ class SuggestedRecipe extends Component {
         return (
           <div className="feature-window">
             <div>render() in SuggestedRecipe.jsx</div>
-            
+
             <div>{this.state.videoTitle}</div>
+            <div><img src={this.state.imageUrl} alt='food' /></div>
             <div className="ingredients">
-                {this.state.ingredients.map((ingredient, i) => (
-                    <div key={i}>{`• ${ingredient.raw_text}`}</div>
-                ))}
+              {this.state.ingredients.map((ingredient, i) => (
+                <div key={i}>{`• ${ingredient.raw_text}`}</div>
+              ))}
+            </div>
+            <div className="instructions">
+              {this.state.instructions.map((instructions, i) => (
+                <div key={i}>{`${i+1}. ${instructions.display_text}`}</div>
+              ))}
             </div>
           </div>
-        )
+        );
   }
 }
 
