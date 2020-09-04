36bec4341462b3aa3002792ea7c3392878ffb3b7
Asher Ferri <asher@Ashers-MBP.fios-router.home>
2020-09-04 01:10:46 -0400

    featured window top card

--- grocery-tracker-app/client/src/components/GroceryItem.jsx
+++ grocery-tracker-app/client/src/components/GroceryItem.jsx
@@ -4,13 +4,13 @@ import Moment from 'moment';
 const GroceryItem = (props) => {
     return (
         <div className="grocery-item-container">
-            <div className=".grcry-item-feat">
-                <h3>{props.grocery.name}</h3>
+            <div>
+                <h3 className="list-name">{props.grocery.name}</h3>
                 {/* Using moment to format date */}
-                <p>Last Purchased: {Moment(props.grocery.last_purchased_date, "dddd, MMMM Do, YYYY").format("M/D/YYYY")}</p> 
-                <p>Shopping Recurrance: {props.grocery.recurrence} days</p>
+                <p className="feat-last">Last Purchased: {Moment(props.grocery.last_purchased_date, "dddd, MMMM Do, YYYY").format("M/D/YYYY")}</p> 
+                <p className="feat-last">Shopping Recurrance: {props.grocery.recurrence} days</p>
                 {/* Using moment to format date and add recurrence */}
-                <p>Expected Re-Purchase: {Moment(props.grocery.last_purchased_date, "dddd, MMMM Do, YYYY").add(props.grocery.recurrence, 'days').format("M/D/YYYY")}</p>
+                <p className="feat-last">Expected Re-Purchase: {Moment(props.grocery.last_purchased_date, "dddd, MMMM Do, YYYY").add(props.grocery.recurrence, 'days').format("M/D/YYYY")}</p>
             </div>
             <div className="item-button">
                 {/* Sending ID back up to GroceryController to lookup single grocery to make edits */}
